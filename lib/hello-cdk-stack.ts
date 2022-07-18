a202a44b9e094ded610f243caf28969963bda0c0
root <root@ip-172-31-13-175.ap-northeast-1.compute.internal>
2022-07-18 13:07:21 +0000

    first commit

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,21 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
  
-     const vpc = new ec2.Vpc(this, 'TheVPC', {
-      cidr: "10.0.0.0/16"
-    })
-
-    const i = new ec2.Instance(this, id, {
-      instanceType: new ec2.InstanceType("t2.medium"),
-      machineImage: new ec2.AmazonLinuxImage,
-      vpc: vpc,
-    })
+    new ecsp.ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      publicLoadBalancer: true
+    });
   }
 }
 
